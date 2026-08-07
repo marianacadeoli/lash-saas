0e82a48944dadaf94099a3813c92c489a60f5197
marianacadeoli <mari_oliveira99@icloud.com>
2026-08-07 00:06:34 -0300

    Corrige logo da tela de cadastro

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,7 +2,6 @@
 
 import { FormEvent, useState } from 'react'
 import Link from 'next/link'
-import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import { createClient } from '@/lib/supabase/client'
 import './register.css'
@@ -108,7 +107,7 @@ export default function RegisterPage() {
 
             <div className="registerCard">
 
-                <Image
+                <img
                     src="/logo.png"
                     alt="PainelEmprest"
                     width={200}
