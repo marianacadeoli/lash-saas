607a5d3ef7ac351ea7b5bdf1fae94c5abd016dbd
marianacadeoli <mari_oliveira99@icloud.com>
2026-08-07 00:01:08 -0300

    Corrige logo da tela de login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,6 @@
 
 import { FormEvent, useState } from 'react'
 import Link from 'next/link'
-import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import { createClient } from '@/lib/supabase/client'
 
@@ -44,14 +43,13 @@ export default function LoginPage() {
 
       <div className="loginCard">
 
-        <Image
-          src="/logo.png"
-          alt="PainelEmprest"
-          width={240}
-          height={60}
-          className="loginLogo"
-          priority
-        />
+<img
+  src="/logo.png"
+  alt="PainelEmprest"
+  width={240}
+  height={60}
+  className="loginLogo"
+/>
 
         <span className="loginBadge">
           Bem-vindo de volta
